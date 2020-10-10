fcbcdd19bd5723d63e19c8951cdc7829a1d3c277
Andy Hablewitz <mail@andyhablewitz.com>
2020-10-10 16:01:57 -0600

    added calibration stript

---
+++ calibration.py
@@ -0,0 +1,24 @@
+from helpers import *
+import time
+
+develop_mode=True
+
+if not develop_mode:
+    water_switch = get_wemo_switch("192.168.86.31")
+
+input('Press enter to start watering')
+
+if not develop_mode:
+    water_switch.on()
+
+start = time.time()
+
+input('Press enter to stop watering')
+
+if not develop_mode:
+    water_switch.off()
+
+end = time.time()
+
+print('Total watering time:')
+print(end - start)
